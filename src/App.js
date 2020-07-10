82d16dc33a1684087c29b7f6bc70bfe9114f8a18
sanachouchan <chouchane.sana.gec@gmail.com>
2020-07-10 11:26:18 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Navbar , Nav , NavDropdown , Button , Form , Col} from 'react-bootstrap'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">Contact Us</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
+
+<h1> Form</h1>
+
+<Form>
+<Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Name</Form.Label>
+      <Form.Control type="text"  placeholder="Enter your name" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Family name</Form.Label>
+      <Form.Control type="text" placeholder="Enter your family name" />
+    </Form.Group>
+  </Form.Row>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+  </Form.Row>
+  <Form.Group controlId="formGridAddress2">
+    <Form.Label>Address </Form.Label>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+  </Form.Group>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Control as="select" value="Choose...">
+        <option>Choose...</option>
+        <option>Sousse</option>
+        <option>Tunis</option>
+        <option>Sfax</option>
+      </Form.Control>
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label>Zip</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group id="formGridCheckbox">
+    <Form.Check type="checkbox" label="I assure my personal informations are correct " />
+  </Form.Group>
+
+  <Button variant="dark" type="submit">
+    Submit
+  </Button>
+</Form>
+
     </div>
   );
 }
---
+++ src/comp.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Comp = () => {
+    return(<div>
+        <h1></h1>
+    </div>)
+}
+export default Comp
